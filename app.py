b36c0a4ce0372f7093a838476c8c15d1f7a91b37
adriankrawczynski <104387768+adriankrawczynski@users.noreply.github.com>
2022-07-02 17:10:17 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,48 +19,53 @@ def licz():
     return render_template('index.html')
 
 @app.route('/wynik', methods=["POST","GET"])
-def calculate():   
-    if request.method == 'POST':
-        e=float(request.form['natężenie_pola'])
-        b1=float(request.form['indukcja_pola_1'])
-        b2=float(request.form['indukcja_pola_2'])
-        v=float(request.form['prędkość_cząstki'])
-        q=float(request.form['ładunek'])
-        r=float(request.form['promień'])
-        if e == v*b1:
-            masa_cala=abs(q)*b2*r*6.02214*(10**19)/v
-            m=round(masa_cala, 4)
-        else:
-            m='nie zmierzona'
-        flash("masa to "+ str(m)+" u")\
-            
-        stepsize=0.01*r
-            
-        if q > 0:
-            x = np.arange(0, r+stepsize, stepsize)
-            y = np.sqrt(r**2 - x**2)
-
-            x = np.concatenate([x,x[::-1]])
-
-            y = np.concatenate([y,-y[::-1]])
-
-            x, y = x, y + r
-        else:
-            x = np.arange(0, r+stepsize, stepsize)
-            y = np.sqrt(r**2 - x**2)
-
-            x = np.concatenate([x,x[::-1]])
-
-            y = np.concatenate([y,-y[::-1]])
-
-            x, y = x , -y - r
-            
-        plt.plot(y, x)
-        plt.savefig('./static/plot.png')
-        plt.close("all")
-
-        return render_template('index.html', url="plot.png")
+def calculate():
+    e=float(request.form['natężenie_pola'])
+    b1=float(request.form['indukcja_pola_1'])
+    b2=float(request.form['indukcja_pola_2'])
+    v=float(request.form['prędkość_cząstki'])
+    q=float(request.form['ładunek'])
+    r=float(request.form['promień'])
+    if abs(e - v*b1)< 1 :
+        masa_cala=abs(q)*b2*r*6.02214*(10**19)/v
+        m=round(masa_cala, 4)
+    else:
+        m='nie zmierzona, ponieważ nie ma cząstka nie porusza się ruchem jednostajnym'
+    flash("masa "+ str(m)+" u")\
     
+    stepsize=0.001*r
+    
+    if q > 0:
+        x = np.arange(0, r+stepsize, stepsize)
+        y = np.sqrt(r**2 - x**2)
+
+        x = np.concatenate([x,x[::-1]])
+
+        y = np.concatenate([y,-y[::-1]])
+
+        x, y = x, y + r
+    else:
+        x = np.arange(0, r+stepsize, stepsize)
+        y = np.sqrt(r**2 - x**2)
+
+        x = np.concatenate([x,x[::-1]])
+
+        y = np.concatenate([y,-y[::-1]])
+
+        x, y = x , -y - r
+    
+    
+    plt.plot([-r*0.01, -r*0.005, r*0.005, r*0.01], [0, r*0.005, r*0.005, 0])
+    plt.plot([-r*2, r*2], [0, 0])
+    plt.plot(y, x)
+    plt.grid(True)
+    plt.xlabel("x cm")
+    plt.ylabel("y cm")
+    plt.title("Wykres ruchu czastki w polu magnetycznym ")
+    plt.savefig('./static/plot.png')
+    plt.close("all")
+
+    return render_template('index.html', url="plot.png")
  
 if __name__ == '__main__':
    app.run(port=5001)
