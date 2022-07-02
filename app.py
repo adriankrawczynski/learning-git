03039e22c61fd232972661623ec97eab4faa7c96
adriankrawczynski <104387768+adriankrawczynski@users.noreply.github.com>
2022-07-02 16:32:31 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from urllib import request
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,19 +9,31 @@ app = Flask(__name__,
 app.secret_key = "dadasdasdas"
 @app.route('/')
 def main():
-    return render_template("strona_glowna.html")
+    return render_template("strona_glowna.html", methods=["POST","GET"])
 @app.route('/tworcy')
 def creators():
-    return render_template("tworcy.html")
-@app.route('/plot', methods=["POST","GET"])
-def plot():
-    #f=request.form['natężenie_pola']
-    #g=request.form['indukcja_pola_1']
-    #h=request.form['indukcja_pola_2']
-    #l=request.form['prędkość_cząstki']
-    r=3
-    q=5
-    stepsize=0.1
+    return render_template("tworcy.html", methods=["POST","GET"])
+
+@app.route('/plot', methods=["POST", "GET"])
+def licz():
+    return render_template('index.html')
+
+@app.route('/wynik', methods=["POST","GET"])
+def calculate():
+    e=float(request.form['natężenie_pola'])
+    b1=float(request.form['indukcja_pola_1'])
+    b2=float(request.form['indukcja_pola_2'])
+    v=float(request.form['prędkość_cząstki'])
+    q=float(request.form['ładunek'])
+    r=float(request.form['promień'])
+    if e == v*b1:
+        masa_cala=abs(q)*b2*r*6.02214*(10**19)/v
+        m=round(masa_cala, 4)
+    else:
+        m='nie zmierzona'
+    flash("masa to "+ str(m)+" u")\
+    
+    stepsize=0.01*r
     
     if q > 0:
         x = np.arange(0, r+stepsize, stepsize)
@@ -43,9 +55,10 @@ def plot():
         x, y = x , -y - r
     
     plt.plot(y, x)
-    plt.savefig('/static/images/plot.png')
+    plt.savefig('./static/plot.png')
+    plt.close("all")
 
-    return render_template('index.html', url='/static/images/plot.png')
-
+    return render_template('index.html', url="plot.png")
+ 
 if __name__ == '__main__':
-   app.run()
+   app.run(port=5001)
